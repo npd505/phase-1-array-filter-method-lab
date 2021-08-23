0e74ca4c02457480f5a8767ca8a0a8f3d35298de
Nora <nora5dempsey@gmail.com>
2021-08-23 10:15:32 -0400

    commit tree 5e32cdf1470a2054d9b63894ab38a809bff9a7d0

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(driversArray, name) {
+    const newDriversArray = driversArray.filter(n => n.toLowerCase() === name.toLowerCase());
+
+    return newDriversArray
+}
+
+
+function fuzzyMatch(driversArray, letters) {
+    const newDriversArray = driversArray.filter(n => n.substring(0, 2) === letters);
+
+    return newDriversArray
+}
+
+
+function matchName(driversObject, name) {
+    const newDriversObj = driversObject.filter(function (n) {return n.name === name});
+
+    return newDriversObj
+}
